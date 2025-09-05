d2a56029b2d2482a6959cdda442330a7495e948f
hoangbao.nguyen <hoangbao.nguyen@asiaesolutions.com>
2025-09-05 16:29:29 +0700

    ,

---
+++ my-app/src/App.js
@@ -0,0 +1,28 @@
+import images from "./asset/images (1).jpg";
+import "./App.css";
+
+function MyButton({user}) {
+  return (
+    <>
+      <button>I'm a button {user.name}</button>
+
+      <img className="classname" src={images} alt="test" />
+    </>
+  );
+}
+function MyApp() {
+  const user = {
+    name: "Hedy Lamarr",
+    imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+    imageSize: 90,
+  };
+  return (
+    <>
+      <div>
+        <h1>Welcome to my app {user.name}</h1>
+        <MyButton user={user}/>
+      </div>
+    </>
+  );
+}
+export default MyApp;
